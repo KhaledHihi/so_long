09c4997b1c7fb9db34d2724aea7d46abe4dcd348
Khaled Hihi <khhihi@e1r3p3.1337.ma>
2025-02-19 19:44:20 +0100

    textures

--- bonus_files/helper_files/image_to_ptr.c
+++ bonus_files/helper_files/image_to_ptr.c
@@ -6,7 +6,7 @@
 /*   By: khhihi <khhihi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 12:14:09 by khhihi            #+#    #+#             */
-/*   Updated: 2025/02/17 17:36:21 by khhihi           ###   ########.fr       */
+/*   Updated: 2025/02/19 19:40:51 by khhihi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,21 @@
 int	file_to_ptr(t_map *elm)
 {
 	int (w), (h);
-	elm->floor = mlx_xpm_file_to_image(elm->mlx, "images/floor.xpm", &w, &h);
-	elm->wall = mlx_xpm_file_to_image(elm->mlx, "images/wall.xpm", &w, &h);
-	elm->player = mlx_xpm_file_to_image(elm->mlx, "images/player.xpm", &w, &h);
-	elm->coin[0] = mlx_xpm_file_to_image(elm->mlx, "images/coin.xpm", &w, &h);
-	elm->coin[1] = mlx_xpm_file_to_image(elm->mlx, "images/2.xpm", &w, &h);
-	elm->coin[2] = mlx_xpm_file_to_image(elm->mlx, "images/3.xpm", &w, &h);
-	elm->coin[3] = mlx_xpm_file_to_image(elm->mlx, "images/4.xpm", &w, &h);
-	elm->coin[4] = mlx_xpm_file_to_image(elm->mlx, "images/5.xpm", &w, &h);
-	elm->coin[5] = mlx_xpm_file_to_image(elm->mlx, "images/6.xpm", &w, &h);
-	elm->coin[6] = mlx_xpm_file_to_image(elm->mlx, "images/7.xpm", &w, &h);
-	elm->coin[7] = mlx_xpm_file_to_image(elm->mlx, "images/8.xpm", &w, &h);
-	elm->coin[8] = mlx_xpm_file_to_image(elm->mlx, "images/9.xpm", &w, &h);
-	elm->coin[9] = mlx_xpm_file_to_image(elm->mlx, "images/10.xpm", &w, &h);
-	elm->enemy = mlx_xpm_file_to_image(elm->mlx, "images/enemy.xpm", &w, &h);
-	elm->exit = mlx_xpm_file_to_image(elm->mlx, "images/exit.xpm", &w, &h);
+	elm->floor = mlx_xpm_file_to_image(elm->mlx, "textures/floor.xpm", &w, &h);
+	elm->wall = mlx_xpm_file_to_image(elm->mlx, "textures/wall.xpm", &w, &h);
+	elm->player = mlx_xpm_file_to_image(elm->mlx, "textures/player.xpm", &w, &h);
+	elm->coin[0] = mlx_xpm_file_to_image(elm->mlx, "textures/coin.xpm", &w, &h);
+	elm->coin[1] = mlx_xpm_file_to_image(elm->mlx, "textures/2.xpm", &w, &h);
+	elm->coin[2] = mlx_xpm_file_to_image(elm->mlx, "textures/3.xpm", &w, &h);
+	elm->coin[3] = mlx_xpm_file_to_image(elm->mlx, "textures/4.xpm", &w, &h);
+	elm->coin[4] = mlx_xpm_file_to_image(elm->mlx, "textures/5.xpm", &w, &h);
+	elm->coin[5] = mlx_xpm_file_to_image(elm->mlx, "textures/6.xpm", &w, &h);
+	elm->coin[6] = mlx_xpm_file_to_image(elm->mlx, "textures/7.xpm", &w, &h);
+	elm->coin[7] = mlx_xpm_file_to_image(elm->mlx, "textures/8.xpm", &w, &h);
+	elm->coin[8] = mlx_xpm_file_to_image(elm->mlx, "textures/9.xpm", &w, &h);
+	elm->coin[9] = mlx_xpm_file_to_image(elm->mlx, "textures/10.xpm", &w, &h);
+	elm->enemy = mlx_xpm_file_to_image(elm->mlx, "textures/enemy.xpm", &w, &h);
+	elm->exit = mlx_xpm_file_to_image(elm->mlx, "textures/exit.xpm", &w, &h);
 	if (!elm->floor || !elm->wall || !elm->player || !elm->coin[0]
 		|| !elm->coin[1] || !elm->coin[2] || !elm->coin[3] || !elm->coin[4]
 		|| !elm->coin[5] || !elm->coin[6] || !elm->coin[6] || !elm->coin[7]
--- helper_files/image_to_ptr.c
+++ helper_files/image_to_ptr.c
@@ -6,7 +6,7 @@
 /*   By: khhihi <khhihi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 12:14:09 by khhihi            #+#    #+#             */
-/*   Updated: 2025/02/14 18:34:47 by khhihi           ###   ########.fr       */
+/*   Updated: 2025/02/19 19:40:18 by khhihi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 int	file_to_ptr(t_map *elm)
 {
 	int (w), (h);
-	elm->floor = mlx_xpm_file_to_image(elm->mlx, "images/floor.xpm", &w, &h);
-	elm->wall = mlx_xpm_file_to_image(elm->mlx, "images/wall.xpm", &w, &h);
-	elm->player = mlx_xpm_file_to_image(elm->mlx, "images/player.xpm", &w, &h);
-	elm->coin = mlx_xpm_file_to_image(elm->mlx, "images/coin.xpm", &w, &h);
-	elm->exit = mlx_xpm_file_to_image(elm->mlx, "images/exit.xpm", &w, &h);
+	elm->floor = mlx_xpm_file_to_image(elm->mlx, "textures/floor.xpm", &w, &h);
+	elm->wall = mlx_xpm_file_to_image(elm->mlx, "textures/wall.xpm", &w, &h);
+	elm->player = mlx_xpm_file_to_image(elm->mlx, "textures/player.xpm", &w, &h);
+	elm->coin = mlx_xpm_file_to_image(elm->mlx, "textures/coin.xpm", &w, &h);
+	elm->exit = mlx_xpm_file_to_image(elm->mlx, "textures/exit.xpm", &w, &h);
 	if (!elm->floor || !elm->wall || !elm->player || !elm->coin || !elm->exit)
 	{
 		return (clean_up(elm), 0);
